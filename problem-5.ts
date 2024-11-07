91b373fafdbdf94bb3eb1ae7cf754074cd474f1a
rifu28 <rifatmeumeu28@gmail.com>
2024-11-07 16:06:07 +0600

    problem-5 solved

--- problem-5.ts
+++ problem-5.ts
@@ -1,2 +1,8 @@
 {
+  function getProperty<T, K extends keyof T>(param1: T, param2: K): T[K] {
+    const output = param1[param2];
+    return output;
+  }
+  const person = { name: "Alice", age: 30 };
+  console.log(getProperty(person, "name"));
 }
